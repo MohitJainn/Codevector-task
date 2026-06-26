6f7210f527b908890391529022093a441ea51a0d
Mohitjain <delipjain123456@gmail.com>
2026-06-26 21:28:31 +0530

    fix port for render

--- backend/index.js
+++ backend/index.js
@@ -57,5 +57,5 @@ app.get('/products', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
